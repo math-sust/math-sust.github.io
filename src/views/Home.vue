c9ef64f5d8409cb0c2b01576f84fcf6ac7554460
math-sust <66895522+math-sust@users.noreply.github.com>
2025-07-15 16:02:03 -0400

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,9 @@
   <div class="home">
     <img alt="Sustainable Studies Logo" src="../assets/iqp_logo.png">
     <h1> WPI Sustainability Resources Platform </h1>
-    <h3> Sustainable Studies IQP Team</h3>
+    <h3> A site for finding courses related to the SDGs</h3>
     <div class="links">
-      <md-button to="/about" class="md-raised md-primary">About the project</md-button>
+      <md-button to="/about" class="md-raised md-primary">MOre on the SDGs and Projectt</md-button>
       <md-button to="/sustainable" class="md-raised md-primary">Sustainability Themed Courses</md-button>
       <md-button to="/faculty" class="md-raised md-primary">Faculty Resources</md-button>
     </div>
